da866979ca12aaa5f8ffa0e82b4f2b02789d3cbe
abe1212 <121511984+abe1212@users.noreply.github.com>
2023-11-09 17:39:36 +0300

    Added solution for Sum of Left Leaves

---
+++ Leetcode-Submissions/sum-of-left-leaves.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node,dxn=''):
+
+            if not node:
+                return 0
+
+            if( not node.left and not node.right and dxn=='L'):
+                return node.val
+
+            return dfs(node.left,'L')+dfs(node.right,'R')
+
+
+        return dfs(root)
+        
